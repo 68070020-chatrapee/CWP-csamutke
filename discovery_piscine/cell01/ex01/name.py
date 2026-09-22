2acc9ff5640cf2a63af4e53d6a76346c809a871f
68070020-chatrapee <68070020@kmitl.ac.th>
2026-09-22 21:09:56 +0700

    cell01

---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Chatrapee"
+last_name = " Samutket"
+whole_name = first_name + last_name
+print(whole_name)
---
+++ discovery_piscine/cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+f = str(input("Hey, what's your first name? : ")).strip()
+l = str(input("And your last name? : ")).strip()
+print("Well, pleased to meet you, " + f + " " + l + ".")
